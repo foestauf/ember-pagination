6f8bdbf7991406e5fd6a8734b60be51db914d5e1
foestauf <46908845+foestauf@users.noreply.github.com>
2020-07-31 09:50:50 -0400

    Typescript support.

--- app/components/articles.js
+++ app/components/articles.js
@@ -1,6 +1,5 @@
 import Component from '@glimmer/component';
-import EmberResolver from 'ember-resolver';
-import Ember from 'ember';
+
 
 export default class ArticlesComponent extends Component {
 
