6addd2d796f80cbcca3830479bb24e0875b55cb3
foestauf <46908845+foestauf@users.noreply.github.com>
2020-07-31 10:55:21 -0400

    Added pagination with ember-cli-pagination

--- app/controllers/articles.js
+++ app/controllers/articles.js
@@ -1,28 +1,5 @@
 import Controller from "@ember/controller";
-import { tracked } from "@glimmer/tracking";
-import { alias, oneWay } from "@ember/object/computed";
-import pagedArray from "ember-cli-pagination/computed/paged-array";
-import { inject } from '@ember/controller'
 
 export default class ArticlesController extends Controller {
-  @inject post
-  // setup our query params
-  queryParams = ["page", "perPage"];
 
-  // set default values, can cause problems if left out
-  // if value matches default, it won't display in the URL
-  @tracked page = 1;
-  @tracked perPage = 10;
-
-  // can be called anything, I've called it pagedContent
-  // remember to iterate over pagedContent in your template
-  @pagedArray('model', {
-    page: alias("parent.page"),
-    perPage: alias("parent.perPage"),
-  })
-  pagedContent;
-
-  // binding the property on the paged array
-  // to a property on the controller
-  @oneWay("pagedContent.totalPages") totalPages;
 }
