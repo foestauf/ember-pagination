4faa0b4cb0aba94e5132514636c363dcffcf0be6
foestauf <46908845+foestauf@users.noreply.github.com>
2020-08-02 09:40:04 -0400

    prettier with husky automation

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,10 +1,10 @@
-import RESTSerializer from "@ember-data/serializer/rest";
+import RESTSerializer from '@ember-data/serializer/rest';
 
 export default RESTSerializer.extend({
   normalizeResponse(store, primaryModelClass, payload, id, requestType) {
-      payload = {posts: payload}
-        console.log(payload);
+    payload = { posts: payload };
+    console.log(payload);
 
-      return this._super(store, primaryModelClass, payload, id, requestType);
-  }
+    return this._super(store, primaryModelClass, payload, id, requestType);
+  },
 });
--- app/serializers/post.js
+++ app/serializers/post.js
@@ -1,13 +1,12 @@
 import RESTSerializer from '@ember-data/serializer/rest';
 
-
 export default RESTSerializer.extend({
   normalizeResponse(store, primaryModelClass, payload, id, requestType) {
     payload = { posts: payload };
     return this._super(store, primaryModelClass, payload, id, requestType);
   },
   normalizeArrayResponse(store, primaryModelClass, payload, id, requestType) {
-    payload.posts.forEach((post) => {
+    payload.posts.forEach(post => {
       post.user = post.userId;
       delete post.userId;
     });
