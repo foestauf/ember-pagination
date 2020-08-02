1cdb5c56318195f12434c58d6edd250fd2649187
foestauf <46908845+foestauf@users.noreply.github.com>
2020-08-02 12:51:10 -0400

    Installation of mirage

--- app/routes/posts.js
+++ app/routes/posts.js
@@ -1,9 +1,9 @@
-// import Route from '@ember/routing/route'
-// import { inject as service } from "@ember/service";
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-// export default class PostsRoute extends Route {
-//     @service store;
-//     async model() {
-//         return this.store.findAll("post");
-//     }
-// }
+export default class PostsRoute extends Route {
+  @service store;
+  async model() {
+    return this.store.findAll('post');
+  }
+}
