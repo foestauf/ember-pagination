a029ca74a54885e8da780a0d487feaea0d32442e
foestauf <46908845+foestauf@users.noreply.github.com>
2020-08-02 18:35:11 -0400

    Adding purgecss back in.

--- tests/unit/models/user-test.ts
+++ tests/unit/models/user-test.ts
@@ -1,7 +1,7 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 import { run } from '@ember/runloop';
-import { get } from '@ember/object';
+// import { get } from '@ember/object';
 
 module('Unit | Model | user', function (hooks) {
   setupTest(hooks);
